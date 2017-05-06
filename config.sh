5ca7f9f5ea0555ad73e842762472c9aa1b813920
AlexCoursi <alexandre.coursimault@gmail.com>
2017-05-06 21:40:12 +0200

    first commit

--- config.sh
+++ config.sh
@@ -1,4 +1,5 @@
-jv_pg_dz_liste_playlists="salsa_2045665684;dimanche_707390125;classique_1431731205;acoustic_1274663331;repas_2631718424;*_2045665684"
-jv_pg_dz_nomBrowser="chromium-browser --new-window"
-jv_pg_dz_nomFenetre="Deezer"
-jv_pg_dz_nbTabLecture=25
+jv_pg_dz_liste_playlists="salsa_2045665684;dimanche_707390125;classique_1431731205;acoustic_1274663331;repas_2631718424;*_2045665684" #liste sous la forme nom de la playliste pour la reconnaissance vocale _ numéro de la playliste deezer
+jv_pg_dz_nomBrowser="chromium-browser --new-window" #ne doit pas être modifié
+jv_pg_dz_nomFenetre="Deezer" #ne doit pas être modifié
+jv_pg_dz_nbTabLecture=25 #à modifier quand deezer modifie leur page
+
