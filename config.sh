917094319386f3086e23ee09937f282c2a891a77
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-05-19 10:48:58 +0200

    config: Be more precise in the explanatation of chromium use

--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
 jv_pg_dz_liste_playlists="detente_1290316405;cocktail_1002095261;danse_2243633142;romantique_645357645;salsa_2045665684;dimanche_707390125;classique_1431731205;acoustic_1274663331;repas_2631718424;*_2045665684" #liste sous la forme nom de la playliste pour la reconnaissance vocale _ numéro de la playliste deezer
-jv_pg_dz_nomBrowser="chromium-browser --new-window" #ne doit pas être modifié
+jv_pg_dz_nomBrowser="chromium-browser --new-window" #ne doit pas être modifié ("chromium --new-window" pour debian hors raspberry)
 jv_pg_dz_nomFenetre="Deezer" #ne doit pas être modifié
 jv_pg_dz_nbTabLecture=25 #NEST PLUS UTILISE - à modifier quand deezer modifie leur page
 jv_pg_dz_offsetHauteurNegativeBoutonPlay=56 #ne doit pas être modifié
