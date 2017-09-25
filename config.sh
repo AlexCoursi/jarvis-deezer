22e07ed8f76fe306a911b95f1d90428ff42cb670
AlexCoursi <alexandre.coursimault@gmail.com>
2017-09-25 19:56:43 +0200

    Ajout des mix et chanson precedente

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
 jv_pg_dz_liste_playlists="detente_1290316405;cocktail_1002095261;dance_2243633142;romantique_645357645;salsa_2045665684;dimanche_707390125;classique_1431731205;acoustic_1274663331;repas_2631718424;ambiance_1086833241;*_2045665684" #liste sous la forme nom de la playliste pour la reconnaissance vocale _ numéro de la playliste deezer
-jv_pg_dz_liste_mix="brésilien_34895;*_34895" #liste sous la forme nom de la playliste pour la reconnaissance vocale _ numéro de la playliste deezer
+jv_pg_dz_liste_mix="bresilien_34895;*_34895" #liste sous la forme nom de la playliste pour la reconnaissance vocale _ numéro de la playliste deezer
 jv_pg_dz_nomBrowser="chromium-browser --new-window" #ne doit pas être modifié ("chromium --new-window" pour debian hors raspberry)
 jv_pg_dz_nomFenetre="Deezer" #ne doit pas être modifié
 jv_pg_dz_offsetHauteurNegativeBoutonPlay=56 #ne doit pas être modifié
 jv_pg_dz_offsetLargeurPositiveBoutonPlay=107 #ne doit pas être modifié
-jv_pg_dz_offsetHauteurPositiveBoutonPlayMix=533 #ne doit pas être modifié
-jv_pg_dz_offsetLargeurPositiveBoutonPlayMix=416 #ne doit pas être modifié
+jv_pg_dz_offsetHauteurPositiveBoutonPlayMix=308 #ne doit pas être modifié
+jv_pg_dz_offsetLargeurPositiveBoutonPlayMix=298 #ne doit pas être modifié
 jv_pg_dz_offsetHauteurNegativeBoutonNext=56 #ne doit pas être modifié
 jv_pg_dz_offsetLargeurPositiveBoutonNext=154 #ne doit pas être modifié
 jv_pg_dz_offsetHauteurNegativeBoutonPrevious=56 #ne doit pas être modifié
